9a60504bbdff77bb466f6f4496a7710e356352c9
AngelL327 <ajlagosp@unah.hn>
2026-02-21 01:57:56 -0600

    c2

--- 02.js
+++ 02.js
@@ -1,20 +1,20 @@
+// numero 2
 function obtenerUsuario(id) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
+  return new Promise(function(resolve) {
+    setTimeout(function() {
       if (id === 1) {
         resolve({ id: 1, nombre: 'Juan' });
       } else if (id === 2) {
         resolve({ id: 2, nombre: 'María' });
       } else {
-        resolve({ id, nombre: 'Desconocido' });
+        resolve({ id: id, nombre: 'Desconocido' });
       }
     }, 1000);
   });
 }
 
-obtenerUsuario(1)
-  .then((usuario1) => {
-    return obtenerUsuario(2).then((usuario2) => {
-      console.log(`Usuarios cargados: ${usuario1.nombre} y ${usuario2.nombre}`);
-    });
+obtenerUsuario(1).then(function(usuario1) {
+  obtenerUsuario(2).then(function(usuario2) {
+    console.log("Usuarios cargados: " + usuario1.nombre + " y " + usuario2.nombre);
   });
+});
