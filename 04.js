59991bcc2fdb543fe53a353305d2fa1ce5ffdd1e
AngelL327 <ajlagosp@unah.hn>
2026-02-21 01:58:57 -0600

    c4

--- 04.js
+++ 04.js
@@ -1,11 +1,12 @@
+// numero 4
 function modificarPersona(persona, nuevoNombre) {
-  return {
-    ...persona,
+  const copia = {
     nombre: nuevoNombre,
     edad: persona.edad + 1
   };
+
+  return copia;
 }
 
 const persona = { nombre: 'Luis', edad: 30 };
-const nuevaPersona = modificarPersona(persona, 'Carlos');
-console.log(nuevaPersona);
+console.log(modificarPersona(persona, 'Carlos'));
