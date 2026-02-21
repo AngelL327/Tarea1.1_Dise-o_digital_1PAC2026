9cc12e22b84b65c67a93bd7035cebaf83d970517
AngelL327 <ajlagosp@unah.hn>
2026-02-21 02:00:52 -0600

    Update 09.js

---
+++ 09.js
@@ -0,0 +1,12 @@
+// numero 9
+function cargarDatos() {
+  return new Promise(function(resolve) {
+    setTimeout(function() {
+      resolve("Datos cargados correctamente");
+    }, 2000);
+  });
+}
+
+cargarDatos().then(function(mensaje) {
+  console.log(mensaje);
+});
